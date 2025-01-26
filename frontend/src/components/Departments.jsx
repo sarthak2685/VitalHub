41f88b679e9084fb9d534164b2f8f001d377756d
KARAN KUMAR GUPTA <karang152btechaiml2025@kccitm.edu.in>
2025-01-26 11:32:16 +0530

    department
    work done

--- frontend/src/components/Departments.jsx
+++ frontend/src/components/Departments.jsx
@@ -1,111 +1,80 @@
 import React from "react";
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import Pediatrics from "../assets/Pediatrician.jpg";
+import Orthopedics from "../assets/Orthopedics.jpg";
+import Cardiology from "../assets/Cardiology.jpg";
+import Neurology from "../assets/Neurology.jpg";
+import Radiology from "../assets/Radiology.jpg";
+import PhysicalTherapy from "../assets/Physical Therapy.jpg";
+import Dermatology from "../assets/Dermatology.jpg";
+import Oncology from "../assets/Oncology.jpg";
+import ENT from "../assets/ENT.jpg";
 
-  const departmentsArray = [
-    {
-      name: "Pediatrics",
-      imageUrl: "/departments/pedia.jpg",
-    },
-    {
-      name: "Orthopedics",
-      imageUrl: "/departments/ortho.jpg",
-    },
-    {
-      name: "Cardiology",
-      imageUrl: "/departments/cardio.jpg",
-    },
-    {
-      name: "Neurology",
-      imageUrl: "/departments/neuro.jpg",
-    },
-    {
-      name: "Oncology",
-      imageUrl: "/departments/onco.jpg",
-    },
-    {
-      name: "Radiology",
-      imageUrl: "/departments/radio.jpg",
-    },
-    {
-      name: "Physical Therapy",
-      imageUrl: "/departments/therapy.jpg",
-    },
-    {
-      name: "Dermatology",
-      imageUrl: "/departments/derma.jpg",
-    },
-    {
-      name: "ENT",
-      imageUrl: "/departments/ent.jpg",
-    },
-  ];
+const departmentsArray = [
+  { name: "Pediatrics", image: Pediatrics },
+  { name: "Orthopedics", image: Orthopedics },
+  { name: "Cardiology", image: Cardiology },
+  { name: "Neurology", image: Neurology },
+  { name: "Oncology", image: Oncology },
+  { name: "Radiology", image: Radiology },
+  { name: "Physical Therapy", image: PhysicalTherapy },
+  { name: "Dermatology", image: Dermatology },
+  { name: "ENT", image: ENT },
+];
 
-
- 
 const responsive = {
-  extraLarge: {
-    breakpoint: { max: 3000, min: 1324 },
-    items: 4,
-    slidesToSlide: 1, // optional, default to 1.
-  },
   large: {
-    breakpoint: { max: 1324, min: 1005 },
+    breakpoint: { max: 3000, min: 1005 },
     items: 3,
-    slidesToSlide: 1, // optional, default to 1.
+    slidesToSlide: 1,
   },
   medium: {
     breakpoint: { max: 1005, min: 700 },
     items: 2,
-    slidesToSlide: 1, // optional, default to 1.
+    slidesToSlide: 1,
   },
   small: {
     breakpoint: { max: 700, min: 0 },
     items: 1,
-    slidesToSlide: 1, // optional, default to 1.
+    slidesToSlide: 1,
   },
 };
 
 const Departments = () => {
   return (
-    <>
-      <div className="">
-        <div className="flex justify-center items-center">
-        <h2 className="text-2xl font-bold mb-6">Departments</h2>
-        </div>
-     
-        <Carousel
-          responsive={responsive}
-          showDots={true}
-          infinite={true}
-          autoPlay={true}
-          autoPlaySpeed={3000}
-          keyBoardControl={true}
-          customTransition="all .5"
-          transitionDuration={500}
-          containerClass="carousel-container"
-          removeArrowOnDeviceType={["tablet", "mobile"]}
-          dotListClass="custom-dot-list-style"
-          itemClass="carousel-item-padding-10-px"
-                  >
-          {departmentsArray.map((depart, index) => (
-            <div
-              key={index}
-              className="card bg-base-100 image-full shadow-xl transform transition-transform duration-300 hover:scale-100 ease-in-out hover:shadow-2xl"
-            >
-              <figure>
-                <img src={depart.imageUrl} alt={depart.name} />
-              </figure>
-              <div className="card-body">
-                <h2 className="card-title">{depart.name}</h2>
-              </div>
-            </div>
-          ))}
-        </Carousel>
+    <div className="pt-8 pb-12">
+      <div className="text-center mb-8">
+        <h2 className="text-gray-500 text-2xl font-bold">Departments</h2>
       </div>
-    </>
+
+      <Carousel
+        responsive={responsive}
+        infinite={true}
+        autoPlay={true}
+        autoPlaySpeed={3000}
+        keyBoardControl={true}
+        customTransition="all .5"
+        removeArrowOnDeviceType={["tablet", "mobile"]}
+      >
+        {departmentsArray.map((depart, index) => (
+          <div
+            key={index}
+            className="relative flex flex-1 justify-center items-end min-h-[320px] bg-white rounded-lg shadow-lg p-5 m-3 text-center hover:shadow-2xl  transition-all duration-300 ease-in-out"
+          >
+            <img
+              src={depart.image}
+              alt={depart.name}
+              className="absolute top-0 left-0 w-full h-full object-cover z-0 rounded-lg"
+            />
+            <div className="bg-gray-300 text-black z-10 uppercase font-bold text-xl w-[280px] py-3 rounded-full">
+              {depart.name}
+            </div>
+          </div>
+        ))}
+      </Carousel>
+    </div>
   );
 };
-
 
 export default Departments;
--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,83 +1,60 @@
 import React from "react";
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { Context } from "../main";
-import { Link, useNavigate } from "react-router-dom";
-
-
+import { useNavigate } from "react-router-dom";
 
 const Hero = ({ title, imageUrl }) => {
-  const { isAuthenticated, setIsAuthenticated } = useContext(Context);
+  const { isAuthenticated } = useContext(Context);
   const navigate = useNavigate();
-
-  /*const handleLogout = async () => {
-    try {
-      const response = await axios.get(
-        "http://localhost:5000/api/v1/user/patient/logout",
-        {
-          withCredentials: true,
-        }
-      );
-      toast.success(response.data.message);
-      setIsAuthenticated(false);
-      navigate("/login");
-    } catch (error) {
-      const message =
-        error.response && error.response.data && error.response.data.message
-          ? error.response.data.message
-          : "Logout failed. Please try again.";
-      toast.error(message);
-    }
-  };*/
 
   const goToLogin = () => {
     navigate("/register");
   };
 
   return (
-    <>
-      <div className="hero containers">
-        <div className="banner ">
-          <h1>{title}</h1>
-          <p className="mt-4">
-            {/*VitalHub is a state-of-the-art facility dedicated to providing 
-            comprehensive healthcare services with compassion and expertise. 
-            Our team of skilled professionals is committed to delivering 
-            personalized care tailored to each patient's needs. 
-            At VitalHub, we prioritize your well-being, ensuring a 
-            harmonious journey towards optimal health and wellness.*/}
-            
-            <b>VITALHUB</b> is more than just a hospital management system; it's a catalyst for improved patient care. By streamlining administrative tasks and providing real-time insights, our platform empowers healthcare providers to deliver exceptional patient experiences. From enhanced communication to efficient resource allocation, <b>VITALHUB</b> is designed to optimize every aspect of your hospital's operations, ultimately leading to better patient outcomes
-          </p>
-
-          <div className="flex flex-col lg:flex-row lg:items-center lg:space-x-4 mt-4 lg:mt-0">
-              {isAuthenticated ? (
-                <button
-                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 mt-6"
-                  onClick={handleLogout}
-                >
-                  Logout
-                </button>
-              ) : (
-                <button
-                  className="text-white bg-black text-xl font-semibold rounded-xl px-4 py-2 cursor-pointer items-center mt-6"
-                  onClick={goToLogin}
-                >
-                  Get Started
-                </button>
-              )}
-              
-          </div>
-          
-        </div>
-
-        <div className="banner">
-          <img src={imageUrl} alt="hero" className="animated-image" />
-          <span>
-            <img src="/Vector.png" alt="vector" />
-          </span> 
+    <div className="flex bg-gray-200 z-[-1] min-h-screen p-20 md:p-24 relative">
+      {/* Left Banner Section */}
+      <div className=" flex flex-col justify-center items-start gap-4">
+        <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-extrabold tracking-wide leading-tight">
+          {title}
+        </h1>
+        <p className="max-w-2xl text-sm sm:text-base md:text-lg text-gray-900 leading-relaxed tracking-wide">
+          <b>VITALHUB</b> is more than just a hospital management system; it's a
+          catalyst for improved patient care. By streamlining administrative
+          tasks and providing real-time insights, our platform empowers
+          healthcare providers to deliver exceptional patient experiences. From
+          enhanced communication to efficient resource allocation,{" "}
+          <b>VITALHUB</b> is designed to optimize every aspect of your
+          hospital's operations, ultimately leading to better patient outcomes.
+        </p>
+        <div className="mt-6">
+          {isAuthenticated ? (
+            <button className="bg-blue-700 text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-6 py-3">
+              Logout
+            </button>
+          ) : (
+            <button
+              className="bg-black text-white text-lg sm:text-xl font-semibold rounded-xl px-6 py-3 mt-6"
+              onClick={goToLogin}
+            >
+              Get Started
+            </button>
+          )}
         </div>
       </div>
-    </>
+
+      {/* Right Banner Section (Image) */}
+      <div className=" flex justify-center items-center relative mt-8 md:mt-0">
+        <img
+          src={imageUrl}
+          alt="hero"
+          className="animate-moveUpDown transition-all ease-in-out duration-1000 max-w-full md:max-w-none"
+        />
+        <span className="absolute top-[-10rem] right-[-10rem] sm:top-[-15rem] sm:right-[-15rem] md:top-[-20rem] md:right-[-20rem] z-[-1]">
+          <img src="/Vector.png" alt="vector" />
+        </span>
+      </div>
+    </div>
   );
 };
 
